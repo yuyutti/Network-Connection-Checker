99555ca9bfd91be1c646d8c4fc361c681439fa5e
yuyutti <yuyutti@yuyutti.com>
2024-04-24 23:56:16 +0900

    Update IP address validation and display logic in client.js

--- public/client.js
+++ public/client.js
@@ -47,14 +47,14 @@ async function info(ipv6Data) {
     GetIPv4.then(ipv4Data => {
         IPv4 = ipv4Data.ip;
         const isValidIPv4 = ipv4Regex.test(IPv4);
-        $('#ipv4-address').text(IPv4 ? IPv4 : '-');
+        $('#ipv4-address').text(isValidIPv4 ? ipv4 : '-');
         $('#ipv4-status').html(isValidIPv4 ? '<span class="text-success">〇</span>' : '<span class="text-danger">✕</span>');
     });
     
     GetIPv6.then(ipv6Data => {
         IPv6 = ipv6Data.ip;
         const isValidIPv6 = ipv6Regex.test(IPv6);
-        $('#ipv6-address').text(IPv6 ? IPv6 : '-');
+        $('#ipv6-address').text(isValidIPv6 ? IPv6 : '-');
         $('#ipv6-status').html(isValidIPv6 ? '<span class="text-success">〇</span>' : '<span class="text-danger">✕</span>');
     });
 
