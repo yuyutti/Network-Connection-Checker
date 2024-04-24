bbebfa7567872d4b4818f746ee7b1f23670e6714
yuyutti <yuyutti@yuyutti.com>
2024-04-24 21:26:26 +0900

    Refactor server configuration and update access IP addresses in client.js and index.js

--- public/client.js
+++ public/client.js
@@ -26,7 +26,7 @@ async function getData(url, timeout = 1000) {
 
 async function info(ipv6Data) {
     info_flag = true
-    let ipInfo = await getData('https://ipinfo.io/' + ip + '?token=' + process.env.TOKEN);
+    let ipInfo = await getData(`https://ipinfo.io/${ip}?token=${process.env.TOKEN}`);
     ipInfo = await ipInfo.json();
     $('#ip').text(ip || '-');
     $('#country').text(ipInfo.Country.companyjp + '(' + ipv6Data.country_name + ')' || '-');
